6b3d90fc96913589b53454a8d7b767bb3efdd100
Yusuf <ybkocaman@gmail.com>
2024-02-07 10:35:25 +0000

    Initial Commit

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Yusuf Burak on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
